e40defc18807dfd553f5b89c0436ffc5aa2b359e
Everthon Souza <everthon.igor@hotmail.com>
2017-10-23 23:04:41 -0200

    Version1.1
    Sistema de Cadastro de Usuários utilizando conceitos de camadas, ASP.Net , ADO.Net, SQL Server e C#

---
+++ SistemaCadastro/Dados/Persistence/Conexao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados.Persistence
+{
+    public class Conexao
+    {
+        //atributos
+        protected SqlConnection con;
+        protected SqlCommand cmd;
+        protected SqlDataReader dr;
+
+        protected void abrirConexao()
+        {
+            try
+            {
+                con = new SqlConnection(@"Data Source=LAPTOP-J868SSLA;Initial Catalog=teste;Integrated Security=True");
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        protected void fecharConexao()
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ SistemaCadastro/Dados/Persistence/UsuarioDAL.cs
@@ -0,0 +1,29 @@
+﻿using Dados.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados.Persistence
+{
+    public class UsuarioDAL : Conexao //herda de conexao para ter acesso aos atributos e metodos
+    {
+        public void GravarUsuario(Usuario u)
+        {
+            try
+            {
+                abrirConexao();
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Erro: " + ex.Message);
+            }
+            finally
+            {
+                fecharConexao();
+            }
+        }
+    }
+}
